c6ea8d3b3cd18209da4e56b678ce4084a94e993d
euri10 <benoit.barthelet@gmail.com>
2016-12-19 17:14:38 +0100

    added pywikidl to download wikipedia in a given lang added pywikicorpus, shamelessly taken from gensim to mangle it, the gensim can only be used as a module which sucks

--- pywikidl/pywikidl.py
+++ pywikidl/pywikidl.py
@@ -1,7 +1,6 @@
 import click
 import re
 import requests
-
 from urllib.parse import urljoin
 from lxml import html
 
@@ -9,7 +8,7 @@ WIKIDL_BASE = "https://dumps.wikimedia.org/"
 
 
 def download_file(url):
-    local_filename = url.split('/')[-1]
+    local_filename = 'results/'+ url.split('/')[-1]
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
     with open(local_filename, 'wb') as f:
@@ -26,19 +25,22 @@ def pywikidl(directory, language):
     """Download wikipedia articles"""
     base_lang = language + "wiki/" + directory
     url = urljoin(WIKIDL_BASE, base_lang)
-
     page = requests.get(url)
     webpage = html.fromstring(page.content)
     all_links = webpage.xpath('//a/@href')
     print(all_links)
     for link in all_links:
         # https://dumps.wikimedia.org/frwiki/latest/frwiki-latest-pages-articles1.xml-p000000003p000412300.bz2
-        pattern = language + "wiki-latest-pages-articles\d+.xml-p\d*p\d*.bz2"
+        # pattern = language + "wiki-latest-pages-articles\d+.xml-p\d*p\d*.bz2$"
+        pattern = language + "wiki-latest-pages-articles\d+.xml-p\d*p\d*.bz2-rss.xml"
         if re.match(pattern=pattern, string=link):
             url_dl = url + '/' + link
+            output = 'results/' + link
+            out_prefix= 'results/' + 'wiki_'+language +'/'
             print(url_dl)
+            print(output)
             download_file(url_dl)
-
 
 if __name__ == '__main__':
     pywikidl()
+
